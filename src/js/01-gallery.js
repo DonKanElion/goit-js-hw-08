542df1998055998ca6e06f54c488248ebfb938fd
Anton Palienko <95186395+DonKanElion@users.noreply.github.com>
2022-10-19 02:10:09 +0300

    Проміжний сейф
    - допрацювати таск 1
    - допрацювати тротл в  таск 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,41 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
 console.log(galleryItems);
+
+const galleryId = document.querySelector("gallery");
+
+const createImgEl = 
+// galleryItems.reduce(
+//     (acc, item) =>
+//       acc +
+//       `<a class="gallery__item" href="${item.original}">
+//     <img class="gallery__image" src="${item.preview}" 
+//     alt="${item.description}" />
+//   </a>`,
+//     ''
+//   );
+
+  // and .MAP
+galleryItems.map(({ preview, original, description})  => 
+
+`<a class="gallery__item" href="${original}">
+<img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`).join('');
+
+galleryId.insertAdjacentHTML("beforeend", createImgEl);
+
+
+// ============ Modal win
+
+// var lightbox = 
+new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionSelector: "img",
+    captionPosition: "bottom",
+    captionsData: "alt",
+    captionDelay: 250,
+});
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,36 @@
 
+import Player from '@vimeo/player';
+
+var throttle = require('lodash.throttle');
+const iframe = document.querySelector('iframe');
+const player = new Player('vimeo-player');
+
+// ====== Save time =====
+
+
+player.on('play', data => {
+    let currentTime = localStorage.setItem("videoplayer-current-time",`${data.seconds}`);
+},);
+
+player.getVideoTitle().then(function(title) {
+    // console.log('title:', title);
+});
+
+//============= PLAY  =======
+
+// player
+//   .setCurrentTime(JSON.parse(localStorage.getItem('videoplayer-current-time')))
+//   .then(function (seconds) {})
+//   .catch(function (error) {
+//     switch (error.name) {
+//       case 'RangeError':
+//         break;
+//       default:
+//         break;
+//     }
+//   });
+
+  // ===   Якщо коротко ===
+
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0 );
+
