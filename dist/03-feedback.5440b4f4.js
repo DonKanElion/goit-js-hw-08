6e47d8ee37c27471b2e03ff7ff249a6c1b23442a
Anton Palienko <95186395+DonKanElion@users.noreply.github.com>
2022-10-20 01:29:56 +0300

    start work task 3

--- dist/03-feedback.5440b4f4.js
+++ dist/03-feedback.5440b4f4.js
@@ -503,6 +503,23 @@ function hmrAcceptRun(bundle, id) {
 }
 
 },{}],"eH52W":[function(require,module,exports) {
+const LOCALSTORAGE_KEY = "feedback-form-state";
+const refs = {
+    form: document.querySelector(".feedback-form"),
+    input: document.querySelector("input"),
+    textarea: document.querySelector("textarea")
+};
+refs.form.addEventListener("input", onFormInput);
+// refs.form.addEventListener("submit", onFormSubmit);
+//form.addEventListener('input', throttle(onFormInput, 500));
+function onFormInput() {
+    const formData = {
+        email: refs.input.value,
+        message: refs.textarea.value
+    };
+    console.log(formData);
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
 
 },{}]},["7Ih8d","eH52W"], "eH52W", "parcelRequire4c75")
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,22 @@
 
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+const refs = {
+    form: document.querySelector(".feedback-form"),
+    input: document.querySelector("input"),
+    textarea: document.querySelector("textarea"),
+}
+
+refs.form.addEventListener("input", onFormInput);
+// refs.form.addEventListener("submit", onFormSubmit);
+
+//form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput () {
+    const formData = { 
+        email: refs.input.value, 
+        message: refs.textarea.value };
+
+    console.log(formData);
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+} 
