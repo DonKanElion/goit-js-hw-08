867a46d83de33ddb1d600291f7fcf817a549564d
Anton Palienko <95186395+DonKanElion@users.noreply.github.com>
2022-10-20 11:39:54 +0300

    the task 3 is solved

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,6 @@
+import throttle from 'lodash.throttle';
 
+// STEP 1 - start
 const LOCALSTORAGE_KEY = 'feedback-form-state';
 
 const refs = {
@@ -7,10 +9,10 @@ const refs = {
     textarea: document.querySelector("textarea"),
 }
 
-refs.form.addEventListener("input", onFormInput);
-// refs.form.addEventListener("submit", onFormSubmit);
+refs.form.addEventListener("input", throttle(onFormInput, 500));
+refs.form.addEventListener("submit", onFormSubmit);
 
-//form.addEventListener('input', throttle(onFormInput, 500));
+
 
 function onFormInput () {
     const formData = { 
@@ -20,3 +22,30 @@ function onFormInput () {
     console.log(formData);
     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
 } 
+
+// STEP 2 - check localStorage
+
+function cheksStorage() {
+    const parseSavedData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+    if (parseSavedData) {
+      refs.input.value = parseSavedData.email;
+      refs.textarea.value = parseSavedData.message;
+    } else {
+      refs.input.value = '';
+      refs.textarea.value = '';
+    }
+  }
+
+  cheksStorage();
+
+  // STEP 3 - submit
+
+  function onFormSubmit(evt) {
+    evt.preventDefault();
+    
+    console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
+    evt.currentTarget.reset();
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+  }
+
