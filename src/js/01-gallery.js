d9bfdd0003769704136d14ec45feed3de6d79ec3
Anton Palienko <95186395+DonKanElion@users.noreply.github.com>
2022-10-22 23:38:45 +0300

    adds 1 and 2 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
 console.log(galleryItems);
+
+const galleryId = document.querySelector(".gallery");
+
+// const createImgEl = galleryItems.map(({ preview, original, description})  => 
+// `<a class="gallery__item" href="${original}">
+// <img class="gallery__image" src="${preview}" alt="${description}" />
+// </a>`).join('');
+
+ // and .reduce
+
+const createImgEl = galleryItems.reduce(
+    (acc, item) =>
+      acc +
+      `<a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" 
+    alt="${item.description}" />
+  </a>`,
+    ''
+  );
+
+galleryId.insertAdjacentHTML("beforeend", createImgEl);
+
+// ============ Modal win
+
+// var lightbox = 
+new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionSelector: "img",
+    captionPosition: "bottom",
+    captionsData: "alt",
+    captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,44 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+// var throttle = require('lodash.throttle');
+// import { throttle } from 'lodash';
+
+const iframe = document.querySelector('iframe');
+const player = new Player('vimeo-player');
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+// ====== Save time =====
+
+// player.on('play', data => {
+//     let currentTime = localStorage.setItem(LOCALSTORAGE_KEY,`${data.seconds}`);
+// });
+
+// const onPlay = function (data) {
+//     const currentTime = localStorage.setItem(LOCALSTORAGE_KEY, data.seconds);
+//   };
+
+onPlay = data => localStorage.setItem(LOCALSTORAGE_KEY, data.seconds);
+player.on('timeupdate', throttle(onPlay, 1000));
+
+// player.getVideoTitle().then(function(title) {
+//     // console.log('title:', title);
+// });
+
+//============= PLAY  =======
+
+// player
+//   .setCurrentTime(JSON.parse(localStorage.getItem('videoplayer-current-time')))
+//   .then(function (seconds) {})
+//   .catch(function (error) {
+//     switch (error.name) {
+//       case 'RangeError':
+//         break;
+//       default:
+//         break;
+//     }
+//   });
+
+  // ===   Якщо коротко ===
+
+  player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY) || 0 );
 
