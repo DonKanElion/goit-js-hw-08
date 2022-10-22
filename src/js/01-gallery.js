7349aa76b81c977d3fcaf1f5f1ec6efeac5d9e12
Anton Palienko <95186395+DonKanElion@users.noreply.github.com>
2022-10-22 23:24:24 +0300

    new Start
    add parcel

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,39 +1,5 @@
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-
+// Add imports above this line
 import { galleryItems } from './gallery-items';
+// Change code below this line
 
 console.log(galleryItems);
-
-const galleryId = document.querySelector(".gallery");
-
-// const createImgEl = galleryItems.map(({ preview, original, description})  => 
-// `<a class="gallery__item" href="${original}">
-// <img class="gallery__image" src="${preview}" alt="${description}" />
-// </a>`).join('');
-
- // and .reduce
-
-const createImgEl = galleryItems.reduce(
-    (acc, item) =>
-      acc +
-      `<a class="gallery__item" href="${item.original}">
-    <img class="gallery__image" src="${item.preview}" 
-    alt="${item.description}" />
-  </a>`,
-    ''
-  );
-
-galleryId.insertAdjacentHTML("beforeend", createImgEl);
-
-// ============ Modal win
-
-// var lightbox = 
-new SimpleLightbox('.gallery a', {
-    captions: true,
-    captionSelector: "img",
-    captionPosition: "bottom",
-    captionsData: "alt",
-    captionDelay: 250,
-});
-
